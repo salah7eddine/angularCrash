b6d5cfd2b6587fc7d4685fba92b3af3b56456d7b
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2021-05-24 15:54:19 +0100

    Angular HTTP Client

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../models/Task';
 import { TASKS } from '../mocks/mock-tasks';
 import { Observable, of } from 'rxjs';
